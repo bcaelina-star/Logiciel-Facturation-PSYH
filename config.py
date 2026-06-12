331126bf5b345d9d80b05978034d1b9fd7eb9a72
bcaelina-star <bcaelina@gmail.com>
2026-06-12 15:06:42 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,23 @@
+# config.py
+MENTIONS = {
+    "cabinet": "Cabinet PsyH",
+    "psychologue": "Céline Bourbon Psychologue",
+    "adresse": "77 rue du Jardin Public, 33000 Bordeaux",
+    "adeli": "339332694",
+    "siret": "82241682200036",
+    "sirene": "Inscription au répertoire Sirene",
+    "declaration": "U63070283920",
+    "site": "www.psyh.fr",
+    "email": "contact@psyh.fr",
+    "tel": "07 56 87 70 51",
+    "tva_note": "TVA non applicable – article 293 B du CGI"
+}
+
+TARIFS = {
+    "adulte": 70.00,
+    "etudiant": 50.00
+}
+DUREE_REFERENCE = "55 minutes"
+LOGO_PATH = "Logo PSYH Celine Bourbon psychologue Bordeaux - Psychologie Humaniste.png"
+
+DATABASE = "factures.db"
